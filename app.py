72b5940e7bcef6858b55cebad7b974a09149d645
git-pixel22 <workemail.rajat@gmail.com>
2023-11-20 20:19:01 +0530

    stuff

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
     elif request.method == "POST":
+        
+        inputAudio = request.form.get("inputAudio")
+        filter = request.form.get("selectedFilter")
+
+        # 1. sent the data to processs to main.py
+
+        # 2. receive the data back from main.py
+
+        # 3. display the data in output.html
+
         return render_template("output.html")
 
