e10a6be6312b0faef3dae724bde1f1b57f7f06cf
git-pixel22 <workemail.rajat@gmail.com>
2023-11-20 18:48:35 +0530

    merged get and post in same decorator

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-
-@app.route("/output", methods=["POST"])
-def output():
-    return render_template("output.html", )
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("output.html")
 
