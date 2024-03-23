ebb308d3a49f02c1774bfa11898e2aed379eb9a8
git-pixel22 <workemail.rajat@gmail.com>
2024-03-23 17:03:28 +0530

    lil css added

---
+++ static/script.js
@@ -0,0 +1,4 @@
+document.getElementById('file-upload').addEventListener('change', function(e) {
+    var fileName = e.target.files[0].name;
+    document.getElementById('file-name').textContent = fileName ? fileName : "No file chosen";
+});
